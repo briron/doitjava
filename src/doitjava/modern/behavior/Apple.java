6e4d2f1253ddd0e8299a498a855ec571329c57c7
kim <youoper@gmail.com>
2021-01-01 16:33:52 +0900

    feat(modern): 임시 객체와 람다를 이용한 filter 기능 추가

--- src/doitjava/modern/behavior/Apple.java
+++ src/doitjava/modern/behavior/Apple.java
@@ -4,6 +4,15 @@ public class Apple {
     private Color color;
     private int weight;
 
+    public Apple(Color color, int weight){
+        this.color = color;
+        this.weight = weight;
+    }
+
+    public String toString(){
+        return "color = " + color + " weight = " + weight;
+    }
+
     public Color getColor() {
         return color;
     }
--- src/doitjava/modern/behavior/AppleFarm.java
+++ src/doitjava/modern/behavior/AppleFarm.java
@@ -1,6 +1,7 @@
 package doitjava.modern.behavior;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class AppleFarm {
@@ -22,6 +23,22 @@ public class AppleFarm {
             }
         }
         return result;
+    }
 
+    public static void main(String[] args) {
+        List<Apple> inventory = Arrays.asList(
+                new Apple(Color.GREEN, 80),
+                new Apple(Color.GREEN, 155),
+                new Apple(Color.RED, 120));
+        List<Apple> greenApples = filterApples(inventory, new AppleGreenColorPredicate());
+        List<Apple> heavyApples = filterApples(inventory, new ApplePredicate() {
+            public boolean test(Apple apple) {
+                return apple.getWeight() > 150;
+            }
+        });
+        List<Apple> lightApples = filterApples(inventory, (Apple apple) -> (apple.getWeight() <= 150));
+        System.out.println("greenApples = " + greenApples);
+        System.out.println("heavyApples = " + heavyApples);
+        System.out.println("lightApples = " + lightApples);
     }
 }
